e9175584a1360235b097ff9171eb386c1621f794
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-17 08:52:40 -0800

    added tests for routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,14 +5,29 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Trip = require('../lib/models/Trip');
+const ItineraryItem = require('../lib/models/ItineraryItem');
+let trip;
+let itineraryItem;
 
 describe('app routes', () => {
   beforeAll(() => {
     connect();
   });
 
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
+  beforeEach(async() => {
+    await mongoose.connection.dropDatabase();
+    trip = await Trip.create({
+      name: 'Ski Trip',
+      start: Date(2020, 1, 10),
+      end: Date(2020, 1, 14)
+    });
+    itineraryItem = await ItineraryItem.create({
+      name: 'Lessons',
+      time: Date(2020, 1, 11),
+      trip: trip._id,
+      lat: 45,
+      long: 120
+    });
   });
 
   afterAll(() => {
@@ -52,8 +67,88 @@ describe('app routes', () => {
           expect(res.body).toContainEqual({
             _id: trip._id.toString(),
             name: trip.name,
-            __v: 0
+            __v: 0,
           });
+        });
+      });
+  });
+
+  it('gets a trip by id', () => {
+    return request(app)
+      .get(`/api/v1/trips/${trip._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Ski Trip',
+          __v: 0,
+          start: JSON.parse(JSON.stringify(trip.start)),
+          end: JSON.parse(JSON.stringify(trip.end))
+        });
+      });
+  });
+
+  it('updates an event by id', async() => {
+    return request(app)
+      .patch(`/api/v1/trips/${trip._id}`)
+      .send({ name: 'Snowboarding Trip' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Snowboarding Trip',
+          start: JSON.parse(JSON.stringify(trip.start)),
+          end: JSON.parse(JSON.stringify(trip.end)),          
+          __v: 0
+        });
+      });
+  });
+
+  it('deletes a trip by id', () => {
+    return request(app)
+      .delete(`/api/v1/trips/${trip._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Ski Trip',
+          start: JSON.parse(JSON.stringify(trip.start)),
+          end: JSON.parse(JSON.stringify(trip.end)),
+          __v: 0
+        });
+      });
+  });
+
+  it('creates an itinerary item', () => {
+    return request(app)
+      .post('/api/v1/itinerary-items')
+      .send({
+        trip: trip._id,
+        time: Date.now(),
+        lat: 45,
+        long: 120
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          trip: trip._id.toString(),
+          time: expect.any(String),
+          lat: 45,
+          long: 120,
+          __v: 0,
+          woeid: 2151330
+        });
+      });
+  });
+
+  it('deletes an itinerary item', async() => {
+    return request(app)
+      .delete(`/api/v1/itinerary-items/${itineraryItem._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          trip: trip._id.toString(),
+          time: expect.any(String),
+          lat: 45,
+          long: 120,
+          __v: 0
         });
       });
   });
